ad6aa9ce4eb2d9ac684fcdb327ea90555970f2ad
Tareq Mia <tareqmia81@gmail.com>
2020-06-23 15:52:09 -0400

    first commit

---
+++ quote_generator/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import QuoteMachine from './components/QuoteMachine';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+	container: {
+		display: 'flex',
+		height: '100vh',
+		alignItems: 'center'
+	}
+};
+
+class App extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			quotes: [],
+			selectedQuoteIndex: null
+		};
+		this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
+		this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+	}
+
+	componentDidMount() {
+		fetch(
+			'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json'
+		)
+			.then((data) => data.json())
+			.then((quotes) => this.setState({ quotes }, this.assignNewQuoteIndex));
+	}
+
+	get selectedQuote() {
+		if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+			return;
+		}
+
+		return this.state.quotes[this.state.selectedQuoteIndex];
+	}
+
+	nextQuoteClickHandler() {
+		console.log('check');
+	}
+
+	selectQuoteIndex() {
+		if (!this.state.quotes.length) {
+			return;
+		}
+		return random(0, this.state.quotes.length - 1);
+	}
+
+	assignNewQuoteIndex() {
+		this.setState({ selectedQuoteIndex: this.selectQuoteIndex() });
+	}
+
+	render() {
+		return (
+			<Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+				<Grid xs={11} lg={8} item>
+					{this.selectedQuote ? (
+						<QuoteMachine
+							selectedQuote={this.selectedQuote}
+							assignNewQuoteIndex={this.assignNewQuoteIndex}
+						/>
+					) : null}
+				</Grid>
+			</Grid>
+		);
+	}
+}
+
+export default withStyles(styles)(App);
---
+++ quote_generator/src/components/QuoteMachine.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+const QuoteMachine = ({ selectedQuote, assignNewQuoteIndex }) => {
+	return (
+		<Card>
+			<CardContent>
+				<Typography id="text">
+					{selectedQuote.quote}
+					<br />- <span id="author">{selectedQuote.author}</span>
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button id="new-quote" size="small" onClick={assignNewQuoteIndex}>
+					New Quote
+				</Button>
+				<IconButton
+					id="tweet-quote"
+					target="_blank"
+					href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}`}
+				>
+					<FontAwesomeIcon icon={faTwitter} size="md" />
+				</IconButton>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default QuoteMachine;
